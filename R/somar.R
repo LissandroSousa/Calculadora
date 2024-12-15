6a2a248ae1a159460e782ea0492de94a1bae3c90
LissandroSousa <lisandrosousa54@gmai.com>
2024-12-14 23:59:20 -0300

    First Commit

---
+++ R/somar.R
@@ -0,0 +1,14 @@
+#' Função para somar.
+#'
+#' Essa soma dois números.
+#'
+#' @param a um número.
+#' @param B outro número
+#'
+#' @examples
+#' somar(3,8)
+#'
+#' @export
+somar <- function(a,b){
+  a + b
+}
---
+++ R/subtrair.R
@@ -0,0 +1,14 @@
+#' Função para subtrair.
+#'
+#' Essa subtrair dois números.
+#'
+#' @param a um número.
+#' @param B outro número
+#'
+#' @examples
+#' somar(8,3)
+#'
+#' @export
+subtrair <- function(a,b){
+  a - b
+}
