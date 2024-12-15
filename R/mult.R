ccac9e67625abcffad4aa4466316fa0165b3a07e
LissandroSousa <lisandrosousa@gmail.com>
2024-12-15 14:54:28 -0300

    second commit

---
+++ R/mult.R
@@ -0,0 +1,15 @@
+#' Função para multiplicar.
+#'
+#' Essa multiplicar dois números.
+#'
+#' @param a um número.
+#' @param B outro número
+#'
+#' @examples
+#' mult(8,3)
+#'
+#' @export
+mult <- function(a,b){
+  a * b
+}
+
